f78a266db64fe7f7a8bb21fad88a7af7f072a4f4
Mementomori666 <bezrukovegor@gmail.com>
2016-08-04 22:31:15 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Егор
+ * Date: 04.08.2016
+ * Time: 22:22
+ */
+<html>
+<head>
+<title>Научный журнал Juvenis scientia</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Егор
+ * Date: 04.08.2016
+ * Time: 22:26
+ */
