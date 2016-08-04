3d5af6882ed0831c5746b2858e175fa34ac8d2aa
Егор <bezrukov.e.a.90@gmail.com>
2016-08-04 23:10:58 +0300

    1

--- index.php
+++ index.php
@@ -5,3 +5,6 @@
  * Date: 04.08.2016
  * Time: 22:26
  */
+include 'header.php';
+include 'menu.php';
+include 'footer.php';
--- menu.php
+++ menu.php
@@ -1,2 +1,10 @@
 <?php
 
+$file = fopen("menu.txt", "r");
+if(!$file)
+    throw new Exception('файл не существует');
+while (!feof($file)){
+    $arrMenu = explode(';',fgets($file));
+    echo "<li><a href='".$arrMenu[1]."'>".$arrMenu[0]."</a>";
+}
+
