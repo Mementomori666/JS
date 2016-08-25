6695527de43187bedbcc75e0018a179b3c7d26ea
temios <drilnmain@gmail.comn>
2016-08-26 01:10:38 +0300

    menu dev 4

--- classes/MainMenuInit.php
+++ classes/MainMenuInit.php
@@ -6,36 +6,41 @@
  * Date: 09.08.2016
  * Time: 17:37
  */
-
-class MainMenuInit
-{
-    public static function mainInit(){
+class MainMenuInit {
+    public static function mainInit() {
         $link = \Connect::getInstance()->getLink();
         $query = '';
         $menuItem = array();
+        $submenus = array();
         $menuInStr = '';
         try {
             $query = "SELECT id, name, href, parent_id, css_class, title FROM Item_Menu;";
-        }catch (Exception $e){
+        } catch (Exception $e) {
             echo $e->getMessage();
         }
         $res = $link->query($query, PDO::FETCH_ASSOC);
         $menuArray = $res->fetchAll();
         $structuredMenu = array();
-        foreach($menuArray as $menuItem){
+        foreach ($menuArray as $menuItem) {
             $structuredMenu[$menuItem['parent_id']][] = $menuItem;
         }
         $parentIds = array_keys($structuredMenu);
-        for($i = count($parentIds); $i > 0 ; $i--){
-            if($parentIds[$i] != 0) {
-                foreach($structuredMenu[$parentIds[$i]] as $item){
-                    $menuItem[$parentIds[$i]][] = new MenuItem($item['id'], $item['name'], $item['href'], $item['parent_id'], $item['css_class']);
+        $parentIds = array_reverse($parentIds);
+        foreach($parentIds as $id){
+            $menuItem = array();
+            foreach ($structuredMenu[$id] as $item) {
+                $submenu = null;
+                if(array_key_exists($item['id'], $submenus)){
+                    $submenu = $submenus[$item['id']];
                 }
-                $submenus[$parentIds[$i]] = new Menu($menuItem);
+                $menuItem[$id][] = new MenuItem($item['id'], $item['name'], $item['href'], $item['parent_id'], $item['css_class'], $submenu);
+
             }
+            $submenus[$id] = new Menu($menuItem);
         }
+        $menu = $submenus[0];
 //        echo '<pre>';
-//        var_dump($menu);
+//        print_r($menu);
 //        echo '</pre>';
         //return $menu->getMenu();
     }
