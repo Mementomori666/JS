ec758366fc1a5f96c8eea444e0f0f4092a10233a
snap492 <mysnap492@gmail.com>
2016-09-26 22:34:03 +0300

    admin not complete

---
+++ admin/classes/Autoload.php
@@ -0,0 +1,20 @@
+<?php
+$root_dir = 'JS.local';
+define('DS', DIRECTORY_SEPARATOR);
+define ('ROOT_DIR', explode($root_dir, __DIR__)[0].$root_dir.DS.'admin'.DS);
+
+
+spl_autoload_extensions('.php');
+spl_autoload_register('loadClasses');
+
+function loadClasses($className) {
+    if (file_exists(ROOT_DIR  . 'controller'. DS . $className . '.php')) {
+        require_once ROOT_DIR . 'controller' . DS. $className . '.php';
+    } elseif (file_exists('model' . $className . '.php')) {
+        require_once ROOT_DIR  . 'model' . DS . $className . '.php';
+    }elseif (file_exists('secure' . $className . '.php')) {
+        require_once ROOT_DIR  . 'secure' . DS . $className . '.php';
+    } else {
+        require_once ROOT_DIR . 'classes' . DS . $className . '.php';
+    }
+}
