89855ab2aab95535684fa6b022622fa1a6c097e5
Ivan Lvov <drilnmain@gmail.com>
2016-11-06 00:17:11 +0300

    issue dev

---
+++ view/issue.php
@@ -0,0 +1,243 @@
+<div id="Layer1">
+    <div id="Layer1_Container">
+        <div id="wb_Text2">
+            <span id="wb_uid0"><strong>Juvenis scientia 2015 № 1</strong></span></div>
+        <div id="wb_Image3">
+            <img src="images/img0053.png" id="Image3" alt=""></div>
+        <div id="wb_CssMenu3">
+            <ul>
+                <li class="firstmain"><a href="./../../files/articles/2015/1/2015-01.pdf" target="_self" title="&#1057;&#1082;&#1072;&#1095;&#1072;&#1090;&#1100; &#1085;&#1086;&#1084;&#1077;&#1088; &#1074; PDF">&#1057;&#1082;&#1072;&#1095;&#1072;&#1090;&#1100;&nbsp;&#1085;&#1086;&#1084;&#1077;&#1088;&nbsp;&#1074;&nbsp;PDF</a>
+                </li>
+                <li><a href="http://ruslibart.com/products/21235164" target="_blank">&#1050;&#1091;&#1087;&#1080;&#1090;&#1100;&nbsp;&#1078;&#1091;&#1088;&#1085;&#1072;&#1083;</a>
+                </li>
+            </ul>
+        </div>
+        <div id="Html2">
+            <script>(function() {
+                    if (window.pluso)if (typeof window.pluso.start == "function") return;
+                    if (window.ifpluso==undefined) { window.ifpluso = 1;
+                        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                        var h=d[g]('body')[0];
+                        h.appendChild(s);
+                    }})();</script>
+            <div class="pluso" data-background="transparent" data-options="small,square,line,horizontal,counter,theme=04" data-services="vkontakte,facebook,odnoklassniki,twitter,email,print"></div></div>
+        <table id="Table1">
+            <tr>
+                <td colspan="3" class="cell0"><span id="wb_uid1"><strong>ФИЗИКА</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid2"><strong>Петров Д. А., Проскурин С. Г.</strong><br><a href="./01/001.html">ОПРЕДЕЛЕНИЕ СКОРОСТИ ПОТОКА ПО ДОПЛЕРОВСКОМУ СДВИГУ В ОПТИЧЕСКОЙ КОГЕРЕНТНОЙ ТОМОГРАФИИ</a></span></td>
+                <td class="cell2"><span id="wb_uid3"> 3 - 5</span></td>
+                <td class="cell3"><span id="wb_uid4"><a href="./01/001.html">Аннотация</a><br><a href="./../../files/articles/2015/1/01_Petrov.pdf">Скачать в PDF</a></span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell0"><span id="wb_uid5"><strong>ХИМИЯ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid6"><strong>Штейнберг Е. М., Зенитова Л. А.</strong><br><a href="./01/002.html">БИОРАЗЛАГАЕМЫЙ МАТЕРИАЛ НА ОСНОВЕ ПОЛИАМИДА И НАТУРАЛЬНОГО КАУЧУКА</a></span></td>
+                <td class="cell2"><span id="wb_uid7"> 6 - 7</span></td>
+                <td class="cell3"><span id="wb_uid8"> <a href="./01/002.html">Аннотация</a><br><a href="./../../files/articles/2015/1/02_Shteinberg.pdf">Скачать в PDF</a></span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell0"><span id="wb_uid9"><strong>ТЕХНИЧЕСКИЕ НАУКИ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid10"><strong>Манзор Д. Э., Тлеугабулов Б. С.</strong><br>РАСЧЕТНО-ТЕОРЕТИЧЕСКОЕ ИССЛЕДОВАНИЕ ЖИДКОФАЗНОЙ ПЕРЕРАБОТКИ СТАЛЕПЛАВИЛЬНЫХ ШЛАКОВ ОАО «ЕВРАЗ НТМК»</span></td>
+                <td class="cell2"><span id="wb_uid11"> 8 - 12</span></td>
+                <td class="cell3"><span id="wb_uid12"> Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid13"><strong>МЕДИЦИНА</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid14"><strong>Мухаммадиева С. М., Ризамухамедова М. З., Ташпулатова М. М.<br></strong>ЭНДОТЕЛИАЛЬНАЯ ДИСФУНКЦИЯ У БОЛЬНЫХ РЕВМАТОИДНЫМ АРТРИТОМ НА ФОНЕ ЛЕЧЕНИЯ МЕТОТРЕКСАТОМ</span></td>
+                <td class="cell2"><span id="wb_uid15">13 - 16</span></td>
+                <td class="cell3"><span id="wb_uid16"> Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid17"><strong>ГЕОГРАФИЯ, НАУКИ О ЗЕМЛЕ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid18"><strong>Мишланова Ю. Л.</strong><br>ОЦЕНКА СТЕПЕНИ ПАРАЗИТИРОВАНИЯ ЭКТОПАРАЗИТОВ НА МЕЛКИХ МЛЕКОПИТАЮЩИХ ЗАПОВЕДНИКА «БАСЕГИ»</span></td>
+                <td class="cell2"><span id="wb_uid19"> </span></td>
+                <td class="cell3"><span id="wb_uid20"> Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid21"><strong>ФИЛОЛОГИЯ И ЖУРНАЛИСТИКА</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid22"><strong>Гаврилова Е. Г.</strong><br>ОТРАЖЕНИЕ НАЦИОНАЛЬНОГО МИРООЩУЩЕНИЯ В РУССКИХ ПОСЛОВИЦАХ И ПОГОВОРКАХ С КОМПОНЕНТАМИ-ЧИСЛИТЕЛЬНЫМИ ДВА И ДВОЕ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid23">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid24"><strong>Ткач А. Ю.</strong><br>НОРМАТИВНЫЙ АСПЕКТ РЕКЛАМНОГО ДИСКУРСА: НАРУШЕНИЕ ЛИТЕРАТУРНЫХ НОРМ КАК ОШИБКА И ПРИЕМ ПРИВЛЕЧЕНИЯ ВНИМАНИЯ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid25">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid26"><strong>ИСТОРИЯ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid27"><strong>Рева Б. В.</strong><br>ОСЕННЯЯ КАЛЕНДАРНАЯ ОБРЯДНОСТЬ ЖИТЕЛЕЙ ЮГО-ЗАПАДНОГО ОПИЛЛЯ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid28">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell11"><span id="wb_uid29"><strong>Егоров Е. В., Цверианашвили И. А.</strong><br>Н. Ф. С. ГРУНДТВИГ И ЕГО ПАМФЛЕТ «ЖЕЛАЕМО ЛИ ОБЪЕДИНЕНИЕ СЕВЕРА? ОБРАЩЕНИЕ К НАРОДУ ШВЕЦИИ»: К ВОПРОСУ ОБ ОБЪЕДИНЕНИИ СЕВЕРОЕВРОПЕЙСКИХ СТРАН</span></td>
+                <td class="cell12">&nbsp;</td>
+                <td class="cell13"><span id="wb_uid30">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid31"><strong>ПЕДАГОГИКА</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid32"><strong>Алейникова О. О.</strong><br>ИНФОРМАЦИОННЫЕ ТЕХНОЛОГИИ В ОБУЧЕНИИ АУДИРОВАНИЮ НА ЗАНЯТИЯХ АНГЛИЙСКОГО ЯЗЫКА</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid33">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid34"><strong>ЭКОНОМИКА И УПРАВЛЕНИЕ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid35"><strong>Шайхутдинов А. М.</strong><br>АНАЛИЗ ПЕРСПЕКТИВНЫХ ТЕХНОЛОГИЙ В ОБЛАСТИ ЭНЕРГЕТИКИ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid36">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid37"><strong>Дмитренко В. В</strong>.<br>БИЗНЕС-ПЛАН - ОДИН ИЗ МЕТОДОВ СТРАТЕГИЧЕСКОГО ПЛАНИРОВАНИЯ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid38">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid39"><strong>Архипова А. В.<br></strong>ПРИМЕНЕНИЕ КРИТЕРИЯ ИРВИНА ДЛЯ АНАЛИЗА ДИНАМИКИ ПРЯМЫХ ИНОСТРАННЫХ ИНВЕСТИЦИЙ В РОССИЙСКОЙ ФЕДЕРАЦИИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid40">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell11"><span id="wb_uid41"><strong>Агеева А. Н. </strong><br>ОЖИДАНИЯ МОЛОДЕЖИ, СВЯЗАННЫЕ С ДЕЯТЕЛЬНОСТЬЮ ОРГАНА МСУ, В КОНТЕКСТЕ «СЕРВИСНОЙ» МОДЕЛИ ГОСУДАРСТВЕННОГО И МУНИЦИПАЛЬНОГО УПРАВЛЕНИЯ</span></td>
+                <td class="cell12">&nbsp;</td>
+                <td class="cell13"><span id="wb_uid42">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid43"><strong>Мытник А. А., Клишин А. П.<br></strong>МОДЕЛИРОВАНИЕ БИЗНЕС-ПРОЦЕССОВ УЧЕБНОГО ПОДРАЗДЕЛЕНИЯ ДЛЯ РАЗРАБОТКИ ИНФОРМАЦИОННОЙ СИСТЕМЫ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid44">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid45"><strong>Ашимова О. Г. </strong><br>ПРЕСС-СЛУЖБА ИННОВАЦИОННЫХ ОРГАНИЗАЦИЙ КАК ЭЛЕМЕНТ ИННОВАЦИОННОЙ ИНФРАСТРУКТУРЫ: ОПЫТ ТОМСКОЙ ОБЛАСТИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid46">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid47"><strong>Ремская К. А.</strong><br>ПРОЕКТНЫЙ МЕТОД КАК ОДИН ИЗ СОВРЕМЕННЫХ МЕТОДОВ ОРГАНИЗАЦИИ ВНЕШНЕЭКОНОМИЧЕСКОЙ ДЕЯТЕЛЬНОСТИ НА ПРЕДПРИЯТИИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid48">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid49"><strong>Зылевич К. А.</strong><br>РАЗВИТИЕ ГОСУДАРСТВЕННОГО ФИНАНСОВОГО КОНТРОЛЯ: АКТУАЛЬНОСТЬ И ПРОБЛЕМЫ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid50">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid51"><strong>Батырова А. Т.</strong><br>РАЗВИТИЕ ИНВЕСТИЦИОННЫХ И ИННОВАЦИОННЫХ ПРОЦЕССОВ В РОССИЙСКОЙ ФЕДЕРАЦИИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid52">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid53"><strong>Демина Ю. К.</strong><br>К ВОПРОСУ ОБ ИМПОРТОЗАМЕЩЕНИИ В РОССИЙСКОЙ ФЕДЕРАЦИИ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid54">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid55"><strong>Комарова Е. О.</strong><br>ПРИМЕНЕНИЕ МСФО В ОБЩЕСТВЕННОМ СЕКТОРЕ РОССИИ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid56">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid57"><strong>Лавренова Е. С. </strong><br>ИНФЛЯЦИЯ КАК ФАКТОР РИСКА В ДЕЯТЕЛЬНОСТИ НЕГОСУДАРСТВЕННЫХ ПЕНСИОННЫХ ФОНДОВ РОССИИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid58">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid59"><strong>Ерыгина Н. С. </strong><br>К ВОПРОСУ ОБ ОЦЕНКЕ ЭФФЕКТИВНОСТИ ПАЕВЫХ ИНВЕСТИЦИОННЫХ ФОНДОВ В РОССИИ</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid60">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell4">&nbsp;</td>
+                <td class="cell5">&nbsp;</td>
+                <td class="cell6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="3" class="cell7"><span id="wb_uid61"><strong>ПРАВОВЕДЕНИЕ</strong></span></td>
+            </tr>
+            <tr>
+                <td class="cell14"><span id="wb_uid62"><strong>Готкевич Г. К.</strong><br>АНАЛИЗ ЗАКОНОПРОЕКТА «О ВЗИМАНИИ ПЛАТЫ В СЧЕТ ВОЗМЕЩЕНИЯ ВРЕДА, ПРИЧИНЯЕМОГО АВТОМОБИЛЬНЫМ ДОРОГАМ ОБЩЕГО ПОЛЬЗОВАНИЯ ФЕДЕРАЛЬНОГО ЗНАЧЕНИЯ ТРАНСПОРТНЫМИ СРЕДСТВАМИ, ИМЕЮЩИМИ РАЗРЕШЕННУЮ МАКСИМАЛЬНУЮ МАССУ СВЫШЕ 12 ТОНН»</span></td>
+                <td class="cell15">&nbsp;</td>
+                <td class="cell16"><span id="wb_uid63">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid64"><strong>Раззоренова И. Н., Вольфсон Э. Н.</strong><br>ОСОБЕННОСТИ МОТИВАЦИИ СОТРУДНИКОВ ПРАВООХРАНИТЕЛЬНЫХ ОРГАНОВ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid65">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell8"><span id="wb_uid66"><strong>Филипповская К. О., Вольфсон Э. Н.</strong><br>КОРРУПЦИЯ: ЕЕ ПРИРОДА, МЕРЫ ПРОФИЛАКТИКИ</span></td>
+                <td class="cell9">&nbsp;</td>
+                <td class="cell10"><span id="wb_uid67">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+            <tr>
+                <td class="cell1"><span id="wb_uid68"><strong>Еремина Ю. И.</strong><br>КРИТЕРИИ ОПРЕДЕЛЕНИЯ ЛИЧНОГО ЗАКОНА И НАЦИОНАЛЬНОСТИ ЮРИДИЧЕСКОГО ЛИЦА</span></td>
+                <td class="cell2">&nbsp;</td>
+                <td class="cell3"><span id="wb_uid69">Аннотация<br>Скачать в PDF</span></td>
+            </tr>
+        </table>
+    </div>
+</div>
+<?php
+$breadcrumbs = new Breadcrumbs();
+echo $breadcrumbs->getBreadcrumbs($breadArr);
+?>
