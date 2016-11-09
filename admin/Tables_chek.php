8279c0767181f70cf760f43429211abe88c241c0
snap492 <mysnap492@gmail.com>
2016-11-09 23:56:13 +0300

    change table_check

--- admin/Tables_chek.php
+++ admin/Tables_chek.php
@@ -6,19 +6,26 @@
  * Time: 19:00
  */
 require_once "classes/Autoload.php";
-try {
-   $link = Connect::getInstance()->getLink();
-   $query = "SELECT * From 'author';";
-   $result = $link->query($query);
-
-   foreach ($result as $row) {
-      var_dump($row);
-//      echo $row['fio_ru'] . "\t";
-//      print $row['fio_en'] . "\t";
-//      print $row[' current_job'] . "\t";
-//      print $row[' article_id'] . "\n";
-   }
-
-} catch (PDOException $e) {
-   echo $e->getMessage();
-}
+//try {
+//   $link = Connect::getInstance()->getLink();
+//   $query = "SELECT * From 'author', 'article';";
+//   $result = $link->query($query, PDO::FETCH_ASSOC);
+//
+//   foreach ($result as $row) {
+//      var_dump($row);
+////      echo $row['fio_ru'] . "\t";
+////      print $row['fio_en'] . "\t";
+////      print $row[' current_job'] . "\t";
+////      print $row[' article_id'] . "\n";
+//   }
+//
+//} catch (PDOException $e) {
+//   echo $e->getMessage();
+//}
+$year = 2016;
+$numMag = 3;
+$data = new DataForMeta();
+$data->setYear($year);
+$data->setNumMag($numMag);
+$result = $data->getArticle();
+var_dump($result);
