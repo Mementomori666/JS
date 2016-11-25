9af7c6476ca00c5e9f091e54c1bc1db77d8a6f11
snap492 <mysnap492@gmail.com>
2016-11-25 23:36:06 +0300

    add style to arch

--- view/arch.php
+++ view/arch.php
@@ -12,10 +12,10 @@
             $str = '';
             foreach ($allReleases as $year){
                 $str .= '<div class="arch_menu_one_year">';
-                $str .= '<div class="arch_menu_year_text">' . $year['pubyear'] . '</div>';
+                $str .= '<div class="arch_menu_year_text">' . $year['pubyear'] . ' год </div>';
                 foreach($year['releases'] as $release){
                     $str .= '<a class="arch_menu_link"
-                        href="/page/arch/' .$year['pubyear'] . '/' . $release['num_mag'] . '">'
+                        href="/page/arch/' .$year['pubyear'] . '/' . $release['num_mag'] . '"> выпуск № '
                         . $release['num_mag'] . '</a>';
                     $str .= '<br>';
                 }
@@ -30,19 +30,20 @@
             $str = '';
             foreach ($articles as $article) {
                 $str .= '<div class="arch_content_article">';
-                if (isset($article['title_ru'])) $str .= '<a class = "arch_content_article_title" href="/page/arch/'.
-                    $pubyear . '/' . $num_mag . '/article/' . $article['id'] .'">' . $article['title_ru'] . '</a>';
-                if (isset($article['title_en'])) $str .= '<a class = "arch_content_article_title">' . $article['title_en'] . '</a>';
-                $str .= '<div class="arch_content_article_authors">';
+                if (isset($article['title_ru'])) $str .= '<div class="article_title"><a class = "arch_content_article_title" href="/page/arch/'.
+                    $pubyear . '/' . $num_mag . '/article/' . $article['id'] .'">' . $article['title_ru'] . '</a> </div>';
+                if (isset($article['title_en'])) $str .= '<div class="article_title"><a class = "arch_content_article_title">' . $article['title_en'] . '</a></div>';
+                $str .= '<div class="arch_content_article_authors"> <span class="author">Автор: </span>
+                                                                    <span class = "author_name">';
                 foreach($article['authors'] as $author){
                     if(isset($author['fio_ru'])) $str .= $author['fio_ru'] . '&nbsp';
                     if(isset($author['fio_en'])) $str .= $author['fio_en'] . '&nbsp';
                 }
-                $str .= '</div>';
+                $str .= '</span> </div>';
                 if(isset($article['article_link']) && ($article['article_link'] != '')) {
                     $linkArr = explode('\\', $article['article_link']);
                     $link = $linkArr[count($linkArr) - 1];
-                    $str .= '<a class="arch_content_download_link" href="/upload/' . $link . '">Скачать статью</a>';
+                    $str .= '<div class="download_link"><a class="arch_content_download_link" href="/upload/' . $link . '">Скачать статью</a></div>';
                 }
                 $str .= '</div>';
             }
